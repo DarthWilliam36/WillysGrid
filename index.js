f935bb7f6061faf6e2219710c16ba155508fb53a
William Martin <will@Williams-MacBook-Air.local>
2023-03-17 15:28:47 -0400

    fixing issues with client recieving files

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const io = new Server(server);
 var fs = require('fs');
 
 var users = 0
-var grid = fs.readFileSync('Grid.json');
+var grid = fs.readFileSync('/Users/will/Desktop/GlobalGrid/Grid.json');
 grid = JSON.parse(grid);
 
-app.use(express.static(__dirname + '/public1'));
+app.use(express.static(__dirname + '/Public'));
 app.use(express.static(__dirname + '/client3.js'));
 
 app.get('/', (req, res) => {
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
         if (msg.x > 39 || msg.x < 0 || msg.y > 39 || msg.y < 0 || msg.color == '') {
             return 
         }
-        if (Number.isInteger(Number(msg.x) == false){
+        if (Number.isInteger(Number(msg.x) == false)) {
             return
         }
         console.log(msg.x + ', ' + msg.y + ': ' + msg.color);
